036932151e1c24370e86a659c553f06954b91966
Surov Vladimir <vovkasu@gmail.com>
2025-07-14 00:13:52 +0300

    Add SerializedPropertyExtensions utility for Unity Editor

---
+++ Assets/PackageSrc/Editor/SerializedPropertyExtensions.cs
@@ -0,0 +1,64 @@
+﻿using UnityEditor;
+using System.Reflection;
+
+namespace LocalizedStringsFinder.Editor
+{
+    public static class SerializedPropertyExtensions
+    {
+        public static object GetTargetObject(this SerializedProperty property)
+        {
+            if (property == null) return null;
+
+            object obj = property.serializedObject.targetObject;
+            var path = property.propertyPath.Replace(".Array.data[", "[");
+
+            var elements = path.Split('.');
+            foreach (var element in elements)
+            {
+                if (element.Contains("["))
+                {
+                    var elementName = element.Substring(0, element.IndexOf("["));
+                    var index = int.Parse(element.Substring(element.IndexOf("[") + 1).Replace("]", ""));
+                    obj = GetValue(obj, elementName, index);
+                }
+                else
+                {
+                    obj = GetValue(obj, element);
+                }
+            }
+
+            return obj;
+        }
+
+        private static object GetValue(object source, string name)
+        {
+            if (source == null)
+                return null;
+
+            var type = source.GetType();
+            var field = type.GetField(name, BindingFlags.NonPublic | BindingFlags.Public | BindingFlags.Instance);
+            if (field == null)
+            {
+                var prop = type.GetProperty(name,
+                    BindingFlags.NonPublic | BindingFlags.Public | BindingFlags.Instance | BindingFlags.IgnoreCase);
+                return prop?.GetValue(source);
+            }
+
+            return field.GetValue(source);
+        }
+
+        private static object GetValue(object source, string name, int index)
+        {
+            var enumerable = GetValue(source, name) as System.Collections.IEnumerable;
+            if (enumerable == null) return null;
+
+            var enumerator = enumerable.GetEnumerator();
+            for (int i = 0; i <= index; i++)
+            {
+                if (!enumerator.MoveNext()) return null;
+            }
+
+            return enumerator.Current;
+        }
+    }
+}
